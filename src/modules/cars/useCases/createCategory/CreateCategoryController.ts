464804fbcad0283fe9ce066c79eb14a0e6955008
Kaic Pierre <kaicpierresilva2002@gmail.com>
2022-05-11 21:48:56 -0300

    uploading docker's files

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -9,7 +9,6 @@ class CreateCategoryController {
     const { name, description } = request.body;
 
     this.createCategoryUseCase.execute({ name, description });
-
     return response.status(201).send();
   }
 }
